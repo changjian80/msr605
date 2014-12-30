e19030279aef4c06f9c8ed985b8abc4819c55d88
jmaxxz <jmaxxz@users.noreply.github.com>
2014-12-30 08:53:23 -0600

    Add setup.py and supporting files for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='msr605',
+      version='1.0',
+      description='An easy to is python interface to the MSR605 card reader',
+      url='https://github.com/steeve/msr605',
+      author='@Steeve, @Jmaxxz, @oxplot',
+      packages=['msr605'],
+      install_requires=[
+      ],
+      zip_safe=False)
